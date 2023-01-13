861b970041bb639bea1d9379e9c8d6a0d9be3846
emin <eminyetim25@gmail.com>
2023-01-13 21:50:30 +0300

    secondCommit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace algoritma1Patika
         public static void Main(string[] args)
         {
             Odevler od = new();
-            od.firstHomeWork();
+            //od.firstHomeWork();
+            od.secondHomework();
         }
     }
 
@@ -28,7 +29,8 @@ namespace algoritma1Patika
                 System.Console.WriteLine("Please enter the " + (i + 1) + ". index :");
                 arr[i] = int.Parse(Console.ReadLine());
             }
-
+            
+            System.Console.WriteLine("** Result **");
             foreach (var item in arr)
             {
                 if(item % 2 == 0)
@@ -36,7 +38,37 @@ namespace algoritma1Patika
                     System.Console.WriteLine(item);
                 }              
             }
-        }  
+        }
+
+        public void secondHomework()
+        {
+            int size = 0;
+            int[] arr;
+            int m = 0;
+
+            System.Console.WriteLine("Please enter the m");
+            m = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Please enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                System.Console.WriteLine("Please enter the " + (i + 1) + ". index :");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            System.Console.WriteLine("** Result **");
+            foreach (var item in arr)
+            {
+                if(item % m == 0 || item == m)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+        }
 
     }
 }
